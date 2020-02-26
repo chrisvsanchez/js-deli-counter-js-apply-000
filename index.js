d99aba411b16b60c96615bb96aac36f01663e1f8
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-26 23:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ function takeANumber(current, name){
     current.push(name);
     return "Welcome, " + name + ". You are number " + (current.length) +" in line.";
 }
+function takeANumber(array, ticketNum){
+  if ( array.length === 0){
+    array.push(1);
+    return "Welcome, you are ticket number 1.";
+  }else{
+     let place = array.push(array.length + 1);
+    return "Welcome, You are ticket number " + place;
+    }
+  }
+
+
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
@@ -21,5 +32,5 @@ function currentLine(katzDeliLine){
       newA.push( " " + (i + 1) + ". " + katzDeliLine[i] );
      }
   }
-  return "The line is currently:" + newA;
+  return "The line is currently:" + newA.join(",");
 }
